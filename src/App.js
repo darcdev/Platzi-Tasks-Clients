2dbe2db6c852b616bc68973e6474f3462270c080
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2020-11-27 09:34:11 -0500

    base enrutador

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Proyectos from "./components/proyectos/Proyectos";
 
 function App() {
-  return <h1>MernTasks</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/nueva-cuenta" component={Register} />
+        <Route exact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
