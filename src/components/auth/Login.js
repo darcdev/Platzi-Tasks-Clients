3d03beb57e2a880f67d0f1a4664f8bf648887d51
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2020-11-27 09:46:35 -0500

    maquetacion y estado form login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,63 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <h1>Login</h1>;
+  //state para iniciar sesion
+  const [usuario, guardarUsuario] = useState({
+    email: "",
+    password: "",
+  });
+  //extraer usuario
+  const { email, password } = usuario;
+  const onChange = (e) => {
+    guardarUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    // validar que no haya campos vacios
+
+    // pasarlo al action
+  };
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesion</h1>
+
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder={password}
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input type="submit" className="btn btn-primario btn-block" />
+          </div>
+        </form>
+
+        <Link to="/nueva-cuenta" className="enlace-cuenta">
+          Obtener Cuenta
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
